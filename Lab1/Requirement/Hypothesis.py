26dc1c8c85b07d0111ff85003b08896cca7c153f
AhmedFouad23 <119745342+AhmedFouad23@users.noreply.github.com>
2026-02-18 14:57:42 +0200

    Refactor HypothesisFunction initialization and execution counts in Matrix Function notebook

--- Lab1/Requirement/Hypothesis.py
+++ Lab1/Requirement/Hypothesis.py
@@ -1,36 +1,26 @@
 import numpy as np
-from typing import Tuple
 
 class HypothesisFunction:
-    l: int
-    m: int
-    k: int
-    Wh: np.ndarray
-    Wo: np.ndarray
-    bh: np.ndarray
-    bo: np.ndarray
-
     def __init__(self, l: int, m: int, k: int):
         # TODO [1]: Initialize the function's unknown parameters
-        self.l = l
-        self.m = m
-        self.k = k
+        self.l: int = l
+        self.m: int = m
+        self.k: int = k
         
         # TODO [2]: Initialize Wh and Wo matrices from a standard normal distribution
-        self.Wh = np.random.randn(m, l)
-        self.Wo = np.random.randn(k, m)
+        self.Wh: np.ndarray = np.random.randn(m, l)
+        self.Wo: np.ndarray = np.random.randn(k, m)
         
-        # TODO [3]: Initialize bo and bh column vectors as zero
-        self.bh = np.zeros((m, 1))
-        self.bo = np.zeros((k, 1))
+        # TODO [3]: Initialize bo and bo column vectors as zero
+        self.bo: np.ndarray = np.zeros((k, 1))
+        self.bh: np.ndarray = np.zeros((m, 1))
 
-    def forward(self, x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    def forward(self, x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         # Ensure input shape matches input size
         assert x.shape[0] == self.l, f"Your input must be consistent the value l={self.l}"
         
-        # TODO [4]: Compute a (Hidden Layer)
-        z_h = np.dot(self.Wh, x) + self.bh
-        a = np.tanh(z_h)
+        # TODO [4]: Compute a as mentioned above
+        a = np.tanh(np.dot(self.Wh, x) + self.bh)
         
         # TODO [5]: Compute output ignoring ReLU
         y = np.dot(self.Wo, a) + self.bo
@@ -53,13 +43,11 @@ class HypothesisFunction:
         
         return z_bar
     
+        # TODO [9]: Annotate all initialized variables and functions above
+        
     def count_params(self) -> int:
         # TODO [10]: Make a lambda function num_params that takes an array z and returns np.prod(z.shape)
         num_params = lambda z: np.prod(z.shape)
-
         # TODO [11]: return the total number of parameters by summing the function over Wh, Wo, bh, bo
-        total_params = (num_params(self.Wh) + 
-                        num_params(self.Wo) + 
-                        num_params(self.bh) + 
-                        num_params(self.bo))
+        total_params = num_params(self.Wh) + num_params(self.Wo) + num_params(self.bh) + num_params(self.bo)
         return total_params
